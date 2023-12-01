c71a5e52d1b299204950cfd6f4484972cc8e50be
chohuison <117262031+chohuison@users.noreply.github.com>
2023-12-01 23:38:25 +0900

    commit

--- src/components/admin/LeagueTableComponent.js
+++ src/components/admin/LeagueTableComponent.js
@@ -1,7 +1,24 @@
 import axios from 'axios'
 const LeaugeTableComponent = ({index,isCreateLeague, toggleModifyLeague,value}) =>{
   const deleteLeague =() => {
-    
+    const apiUrl = `${process.env.REACT_APP_DB_HOST}`+`/api/v1/admin/ctf/league/${value.leaguePk}`;
+      axios.delete(apiUrl,{
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("access")}`,
+        },
+      })
+        .then(response => {
+          console.log(response.data)
+          window.location.reload();
+        
+        
+        })
+        .catch(error => {
+          console.error('리그 삭제 중 에러:', error);
+        });
+
+
 
   }
     return(
@@ -13,7 +30,7 @@ const LeaugeTableComponent = ({index,isCreateLeague, toggleModifyLeague,value}) 
                   <div className="admin-league-table-modify-img"></div>
                 </th>
                 <th className="admin-league-table-delete">
-                  <div className="admin-league-table-delete-img"></div>
+                  <div className="admin-league-table-delete-img" onClick={deleteLeague}></div>
                 </th>
               </tr>
     );
